429094ed388767166953c29179ceaf337e1ffd6e
Andrew <taradeevandrej15@gmail.com>
2026-04-29 00:41:36 +0300

    done task, that is, create 3 files

---
+++ src/finite_automaton.h
@@ -0,0 +1,14 @@
+#ifndef FINITE_AUTOMATON_H
+#define FINITE_AUTOMATON_H
+
+typedef enum { 
+    Q0, Q1, Q2, Q3, Q4, Q5, Q6, ERR 
+} State;
+
+int get_char_class(char c);
+
+int match(const char* str);
+
+void print_transition_table(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
+#include "finite_automaton.h"
 #include <stdio.h>
 
-int main(void)
-{
-    printf("This is main.c\n");
+int main() {
+
+    print_transition_table();
+    
+    printf("\n=== ПРОВЕРКА РАБОТЫ АВТОМАТА ===\n");
+    
+    const char* tests[] = {
+        "123",
+        "456.789",
+        "123E45", 
+        "123E+45",
+        "123E-45",
+        "1.",     
+        ".123",
+        "E123",
+        "123E" 
+    };
+    
+    int n = sizeof(tests) / sizeof(tests[0]);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%-12s -> %s\n", tests[i], match(tests[i]) ? "ПРИНЯТЬ" : "ОТКЛОНИТЬ");
+    }
+    
     return 0;
 }
