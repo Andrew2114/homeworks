20e41b385059412961b802acdd96b41530e13b5d
Andrew <taradeevandrej15@gmail.com>
2026-04-29 00:46:03 +0300

    fix files

--- src/finite_automaton.h
+++ src/finite_automaton.h
@@ -1,8 +1,15 @@
 #ifndef FINITE_AUTOMATON_H
 #define FINITE_AUTOMATON_H
 
-typedef enum { 
-    Q0, Q1, Q2, Q3, Q4, Q5, Q6, ERR 
+typedef enum {
+    Q0,
+    Q1,
+    Q2,
+    Q3,
+    Q4,
+    Q5,
+    Q6,
+    ERR
 } State;
 
 int get_char_class(char c);
